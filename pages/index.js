0fcd0101e814936ca2f04fe2965487ab1aeccb91
lsicaro <151783600+lsicaro@users.noreply.github.com>
2023-11-28 14:06:24 +0000

    pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 function Home(){
     return( 
-    <><h1>Futuro site do Impacto aqui... paciencia com o dev</h1><p>Lideranca: Aquele que salva </p></>
+    <><h1>Futuro site do Impacto aqui... paciencia com o dev</h1>
+    <p>Lideranca:</p>
+    <p>Aquele que salva</p>
+    <p>Paraguaio Japones</p>
+    <p>Pai barrigudo</p>
+    <p>Um dos bes</p>
+    <p>e so</p>
+    </>
     );
 }
 
